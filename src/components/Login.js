153bb54e92f8c88ed36c02fe64b9b420a03033b3
Deepanshu <dp5114@dseu.ac.in>
2023-07-20 16:55:44 +0530

    h

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>
-      aafafafadfdafs
+    <div className='flex space-x-5 font-bold bg-black text-white text-[50px] h-[100vh]' >
+      <div>
+      aafafafadfdafs</div>
+      <button className='px-[4px] py-[2px] border border-white  h-20 w-52 bg-red-700 rounded-[10px]'>start</button>
     </div>
   )
 }
