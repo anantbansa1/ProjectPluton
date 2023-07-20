f7cc7835abb3e29228678aeae6e1de13ac41f203
anant <ab6157@dseu.ac.in>
2023-07-20 16:00:41 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Changepassotp from "./components/Changepassotp";
 
 function App() {
   return (
     <>
-      <div className="bg-[#232323] items-center justify-center flex h-[100vh]">
-        {/* <h1 className="text-3xl font-bold underline">email and otp</h1> */}
-        <div className="flex w-[450px] h-[500px] rounded-lg bg-white margin ">
-          enter otp
-          {/* <h1 className="text-3xl font-bold underline">email and otp</h1>  */}
-        </div>
-      </div>
-      
+      <Changepassotp Email="ab6157@dseu.ac.in"></Changepassotp>
     </>
   );
 }
---
+++ src/components/Changepassotp.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function changepassotp(props) {
+  return (
+    <div>
+      <div className="flex bg-[#232323] items-center justify-center h-[100vh]">
+        <div className="flex w-[450px] h-[500px] rounded-lg bg-white m-auto flex-col items-center">
+          <div className="flex font-bold text-2xl mt-[13vh] "> Enter OTP</div>
+          <div className="flex font-bold text-2xl mt-[5vh] ">
+            Email : {props.Email}
+          </div>
+          <div className="flex mt-[5vh] border-2 border-black">
+            <textarea name="otp" id="otp" cols="10" rows="1"></textarea>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
