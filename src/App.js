e765f840fe6093e57e20285314bee252b6695600
ImDoubD <toodoubd@gmail.com>
2023-07-20 15:02:43 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">email and otp</h1>
+      <div className="bg-[#232323] items-center justify-center flex h-[100vh]">
+        {/* <h1 className="text-3xl font-bold underline">email and otp</h1> */}
+        <div className="flex w-[450px] h-[500px] rounded-lg bg-white margin ">
+          enter otp
+          {/* <h1 className="text-3xl font-bold underline">email and otp</h1>  */}
+        </div>
+      </div>
+      
     </>
   );
 }
