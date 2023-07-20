5747b59d2bd8976ac1a8bf2f860d667dea1b6339
ImDoubD <toodoubd@gmail.com>
2023-07-20 18:52:15 +0530

    seventh commit

--- src/components/Changepassotp.js
+++ src/components/Changepassotp.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField } from "@mui/material";
 
-export default function changepassotp(props) {
+
+
+export default function Changepassotp(props) {
+  const[otp,set_otp] = useState('');
+  const Handle_OTP=(event) => {
+    const newvalue = event.target.value;
+    set_otp(newvalue);
+    
+  } 
+  const Submit_OTP=(event) => {
+    console.log(otp);
+  } 
+  const Resend_OTP=(event) => {
+    console.log(otp);
+  }
+  const Back_OTP=(event) => {
+    console.log(otp);
+  }
+  
   return (
     <div>
       <div className="flex bg-[#232323] items-center justify-center h-[100vh]">
         <div className="flex w-[450px] h-[500px] rounded-lg bg-white m-auto flex-col ">
           <div className="flex justify-start mx-3 my-2">
-            <button>back</button>
+            <button onClick={Back_OTP}>back</button>
           </div>
           <div className="flex justify-center flex-col items-center">
             <div className="flex font-bold text-2xl text-center  mt-[10vh] ">
@@ -23,15 +41,17 @@ export default function changepassotp(props) {
                   label="Outlined"
                   variant="outlined"
                   color="primary"
+                  value={otp}
+                  onChange={Handle_OTP}
                 />
               </div>
             </div>
             <div className="flex mt-[5vh] ">
               <div className="bg-black px-10 py-2 text-white">
-                <button>Submit</button>
+                <button onClick={Submit_OTP}>Submit</button>
               </div>
               <div className="bg-black px-6 py-2 mx-4 text-white">
-                <button>Resend OTP</button>
+                <button onClick={Resend_OTP}>Resend OTP</button>
               </div>
               <div />
             </div>
