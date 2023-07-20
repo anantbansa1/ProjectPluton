a1a0f585592e32f63629ad21b0af89c26083f2be
Deepanshu <dp5114@dseu.ac.in>
2023-07-20 14:34:38 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import loginpage from "./components/loginpage";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <loginpage/>
+
     </>
   );
 }
