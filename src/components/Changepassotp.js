f6997ec9f6509e6f1f8d7938d46389df4b1927d6
anant <ab6157@dseu.ac.in>
2023-07-20 16:23:28 +0530

    button

--- src/components/Changepassotp.js
+++ src/components/Changepassotp.js
@@ -9,8 +9,18 @@ export default function changepassotp(props) {
           <div className="flex font-bold text-2xl mt-[5vh] ">
             Email : {props.Email}
           </div>
-          <div className="flex mt-[5vh] border-2 border-black">
-            <textarea name="otp" id="otp" cols="10" rows="1"></textarea>
+          <div className="flex mt-[5vh]  ">
+            <div className="border-[1px] border-black">
+              <textarea name="otp" id="otp" cols="30" rows="1"></textarea>
+            </div>
+          </div>
+          <div className="flex mt-[5vh]">
+            <div className="bg-black px-10 py-2 text-white">
+              <button>Submit</button>
+            </div>
+            <div className="bg-black px-6 py-2 mx-4 text-white">
+              <button>Resend OTP</button>
+            </div>
           </div>
         </div>
       </div>
