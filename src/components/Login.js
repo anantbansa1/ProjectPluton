e1d9792682ec60c68960a87e05bdb93436c32f26
Madddynaks <madhav.nakra2002@gmail.com>
2023-07-20 16:55:18 +0530

    background

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,9 +3,12 @@ import React from 'react'
 function Login() {
   return (
     <div>
-      aafafafadfdafs
+      <div className='bg-[#a7c7e7] h-[100vh] overflow-hidden'>
+        <div className='bg-[#232323] h-[100vh] scale-150 w-[42%] rotate-[-10deg]'>
+          
+        </div>
+      </div>
     </div>
   )
 }
-
 export default Login
