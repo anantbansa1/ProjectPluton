2f21bde3fa552b8f7cb866fd6eda66f6484477be
Madddynaks <madhav.nakra2002@gmail.com>
2023-07-22 12:54:26 +0530

    finalLogin

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,40 @@
 import React from "react";
 import { TextField } from "@mui/material";
+import { useState } from "react";
 
 function Login() {
+  const [Email, setEmail] = useState("");
+  const [Password, setPass] = useState("");
+  function handlePass(e) {
+    setPass(e.target.value);
+    console.log(Password);
+  }
   return (
     <div>
-      <div className="bg-[#a7c7e7] h-[100vh] flex justify-between overflow-hidden">
-        <div className="bg-[#232323] h-[100vh] scale-150 w-[39%] rotate-[-10deg]"></div>
-        <div className="px-4 items-end bg-white lg:h-[55vh] lg:w-[26vw] h-[300px] w-[300px] flex-col my-auto mr-[8vw] rounded-[10px] shadow-md shadow-black z-10 flex">
-          <div className="mx-auto my-16 font-bold text-3xl">Sign in</div>
-          <div className="mt-4 w-[100%]">
+      <div className="sm:bg-[#a7c7e7] sm:h-[100vh] lg:flex lg:justify-between sm:overflow-hidden max-[640px]: bg-[#232323] max-[640px]: h-[500vh]">
+        <div className="bg-[#232323] h-[100vh] z-0 scale-150 w-[39%] rotate-[-10deg]"></div>
+        <div className=" max-[1024px]:absolute top-[10%] left-[20%] px-4 items-end bg-white max-[1024px]:w-[40vw] max-[640px]:w-[60vw] lg:w-[28vw] lg:flex-col my-auto mx-auto lg:mr-[8vw] rounded-[10px] shadow-md shadow-black z-2  lg:flex">
+          <div className="mx-auto my-16 text-center font-bold text-3xl">Sign in</div>
+          <div className="mt-1 w-[100%]">
             <TextField
               fullWidth
               id="outlined-basic"
               label="Email ID"
               variant="outlined"
+              value={Email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
             />
           </div>
           <div className="mt-6 w-[100%]">
-            <TextField
+            <TextField type="password"
               fullWidth
               id="outlined-basic"
               label="Password"
               variant="outlined"
+              value={Password}
+              onChange={handlePass}
             />
           </div>
 
