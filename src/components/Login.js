50637f88bf72f9c5751b206028c919a38c6a342b
Madddynaks <madhav.nakra2002@gmail.com>
2023-07-22 12:02:38 +0530

    changge

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,36 @@
 import React from "react";
 import { TextField } from "@mui/material";
 
-
 function Login() {
   return (
     <div>
       <div className="bg-[#a7c7e7] h-[100vh] flex justify-between overflow-hidden">
         <div className="bg-[#232323] h-[100vh] scale-150 w-[39%] rotate-[-10deg]"></div>
-        <div className="bg-white lg:h-[65vh] lg:w-[26vw] h-[300px] w-[300px] my-auto mr-[8vw] rounded-[10px] shadow-md shadow-black z-10 flex">
-          <div className="mx-auto my-3 text-xl">Sign in</div>
-          <CssTextField
-            label="Username"
-            className="username"
-            name="username"
-            onChange={this.onChange}
-            type="text"
-            autoComplete="current-password"
-            margin="normal"
-            inputProps={{ style: { fontFamily: "nunito", color: "white" } }}
-          />
+        <div className="px-4 items-end bg-white lg:h-[55vh] lg:w-[26vw] h-[300px] w-[300px] flex-col my-auto mr-[8vw] rounded-[10px] shadow-md shadow-black z-10 flex">
+          <div className="mx-auto my-16 font-bold text-3xl">Sign in</div>
+          <div className="mt-4 w-[100%]">
+            <TextField
+              fullWidth
+              id="outlined-basic"
+              label="Email ID"
+              variant="outlined"
+            />
+          </div>
+          <div className="mt-6 w-[100%]">
+            <TextField
+              fullWidth
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+            />
+          </div>
+
+          <button className="bg-[#232323] p-3 mt-10 w-[100%] mx-auto text-white ">
+            Sign in
+          </button>
+          <button className="p-2 pt-4 mt- w-auto  text-[#232323] hover:underline">
+            Forgot Password?
+          </button>
         </div>
       </div>
     </div>
