1164f087363fd9b21f63e0dac849bc1893625aa1
ImDoubD <toodoubd@gmail.com>
2023-07-25 14:45:08 +0530

    1st commit Addtext

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Navbar from "./components/Navbar";
+import Text from "./components/pages/Text";
 function App() {
   return (
     <>
-      <Navbar></Navbar>
+      <Text></Text>
     </>
   );
 }
