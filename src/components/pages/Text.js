7d4ddd13f7a2a06ccab69868190da6aeab7693b8
Madddynaks <madhav.nakra2002@gmail.com>
2023-07-25 15:13:05 +0530

    changes ho gai

--- src/components/pages/Text.js
+++ src/components/pages/Text.js
@@ -4,7 +4,28 @@ export default function Text() {
   return (
     <div>
       <Navbar></Navbar>
-      hello
+      <div className=' text-white ml-[35vw] mr-[15vw] flex justify-between items-center justify-center mt-[15vh] text-3xl'>
+        <button className='hover:bg-[#4B4A4A] rounded-sm p-[1vh]'>
+          Text
+        </button>
+        <button className='hover:bg-[#4B4A4A] rounded-sm p-[1vh]'>
+          Images
+        </button>
+        <button className='hover:bg-[#4B4A4A] rounded-sm p-[1vh]'>
+          Polls
+        </button>
+        </div>
+      <div className='bg-[#4B4A4A] ml-[28vw] h-[19vw] w-[64vw] mt-[5vw]'></div>
+      <div className='ml-[28vw] text-black text-xl space-x-8 mt-[2vh]'>
+        <button className='bg-white p-[1vh] px-[5vh] rounded-full'>+Event</button>
+        <button className='bg-white p-[1vh] px-[5vh] rounded-full'>+Announcement</button>
+        <button className='bg-white p-[1vh] px-[5vh] rounded-full'>+Achievement</button>
+      </div>
+      <div className='ml-[87vw] text-white text-xl mt-[6vh]'>
+        <button>
+          Submit
+        </button>
+      </div>
     </div>
   )
 }
