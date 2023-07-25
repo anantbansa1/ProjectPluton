a37e6b67cd19e3451a18297a180eea2b95c0ede0
Madddynaks <madhav.nakra2002@gmail.com>
2023-07-25 17:37:33 +0530

    final changes

--- src/components/pages/Text.js
+++ src/components/pages/Text.js
@@ -1,31 +1,94 @@
-import React from 'react'
+import React, { useState } from "react";
 import Navbar from "../Navbar";
 export default function Text() {
+  const [option, setoption] = useState("text");
+  const [event, setevent] = useState("");
+  const [announcement, setannouncement] = useState("");
+  const [achievement, setachievement] = useState("");
+  const [submit, setsubmit] = useState("");
   return (
     <div>
       <Navbar></Navbar>
-      <div className=' text-white ml-[35vw] mr-[15vw] flex justify-between items-center mt-[15vh] text-3xl'>
-        <button className='hover:bg-[#4B4A4A] text-lightgray rounded-full p-[1vh]  px-[3vh]'>
-          Text
-        </button>
-        <button className='hover:bg-[#4B4A4A] rounded-full p-[1vh]  px-[3vh]'>
-          Images
-        </button>
-        <button className='hover:bg-[#4B4A4A] rounded-full p-[1vh]  px-[3vh]'>
-          Polls
-        </button>
+      <div className=" text-white ml-[35vw] mx-auto mr-[10vw] flex space-x-40 items-center mt-[15vh] text-3xl">
+        <div className="flex-col">
+          <button
+            onClick={(e) => {
+              setoption("text");
+            }}
+            className=" text-lightgray rounded-full p-[1vh]  px-[3vh]"
+          >
+            Text
+          </button>
+          {option === "text" ? (
+            <hr className="w-[7vw] border-white border-[2px]" />
+          ) : (
+            <hr className="hidden" />
+          )}
         </div>
-      <div className='bg-[#4B4A4A] ml-[28vw] h-[19vw] w-[64vw] mt-[5vw]'></div>
-      <div className='ml-[28vw] text-black text-xl space-x-8 mt-[2vh]'>
-        <button className='bg-white p-[1vh] px-[5vh] rounded-full'>+Event</button>
-        <button className='bg-white p-[1vh] px-[5vh] rounded-full'>+Announcement</button>
-        <button className='bg-white p-[1vh] px-[5vh] rounded-full'>+Achievement</button>
+        <div className="flex-col">
+          <button
+            onClick={(e) => {
+              setoption("image");
+            }}
+            className=" text-lightgray rounded-full p-[1vh]  px-[3vh]"
+          >
+            Posts
+          </button>
+          {option === "image" ? (
+            <hr className="w-[8vw] border-white border-[2px]" />
+          ) : (
+            <hr className="hidden" />
+          )}
+        </div>
+        <div className="flex-col">
+          <button
+            onClick={(e) => {
+              setoption("Poll");
+            }}
+            className="text-lightgray rounded-full p-[1vh]  px-[3vh]"
+          >
+            Poll
+          </button>
+          {option === "Poll" ? (
+            <div className="bg-blue-400">
+            <hr className="w-[6.5vw] border-white border-[2px]" />
+            </div>
+          ) : (
+            <hr className="hidden" />
+          )}
+        </div>
       </div>
-      <div className='ml-[87vw] text-white text-2xl mt-[6vh]'>
-        <button>
-          Submit
+      <div className="bg-[#4B4A4A] ml-[28vw] h-[19vw] w-[64vw] mt-[5vw]"></div>
+      <div className="ml-[28vw] text-black text-xl space-x-8 mt-[2vh]">
+        <button 
+        onClick={(e) => {
+          setevent(console.log(1));
+        }}
+        className="bg-white p-[1vh] px-[5vh] rounded-full hover:text-red-500">
+          +Event
         </button>
+        <button 
+        onClick={(e) => {
+          setevent(console.log(2));
+        }}
+        className="bg-white p-[1vh] px-[5vh] rounded-full hover:text-red-500">
+          +Announcement
+        </button>
+        <button 
+        onClick={(e) => {
+          setevent(console.log(3));
+        }}
+        className="bg-white p-[1vh] px-[5vh] rounded-full hover:text-red-500">
+          +Achievement
+        </button>
+      </div>
+      <div 
+      onClick={(e) => {
+        setevent(console.log(4));
+      }}
+      className="ml-[87vw] text-white text-2xl mt-[6vh] hover:font-semibold">
+        <button>Submit</button>
       </div>
     </div>
-  )
+  );
 }
