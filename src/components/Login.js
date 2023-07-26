79bca668c03e20dbe5f23ea74e66240a5edd79c3
Madddynaks <madhav.nakra2002@gmail.com>
2023-07-26 12:30:51 +0530

    asodj

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,7 +11,7 @@ function Login() {
   }
   return (
     <div>
-      <div className="sm:bg-[#a7c7e7] sm:h-[100vh] lg:flex lg:justify-between relative overflow-y-hidden max-[640px]: bg-[#232323] max-[640px]: h-[500vh]">
+      <div className="sm:bg-[#a7c7e7] h-[100vh] lg:flex lg:justify-between overflow-y-hidden max-[640px]: bg-[#232323]">
         <div className="bg-[#232323] h-[100vh] z-0 scale-150 w-[39%] rotate-[-10deg] overflow-hidden"></div>
         <div className=" max-[1024px]:absolute top-[10%] left-[20%] px-4 items-end bg-white max-[1024px]:w-[40vw] max-[640px]:w-[60vw] lg:w-[28vw] lg:flex-col my-auto mx-auto lg:mr-[8vw] rounded-[10px] shadow-md shadow-black z-2  lg:flex overflow-hidden">
           <div className="mx-auto my-16 text-center font-bold text-3xl">Sign in</div>
