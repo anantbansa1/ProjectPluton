b85b93ae8278ce38c72b9c4369ad6fefa634d899
anant <ab6157@dseu.ac.in>
2023-07-26 14:00:27 +0530

    logofixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,47 +6,61 @@ import LiveHelpIcon from "@mui/icons-material/LiveHelp";
 import LeaderboardIcon from "@mui/icons-material/Leaderboard";
 import InfoIcon from "@mui/icons-material/Info";
 import PowerSettingsNewIcon from "@mui/icons-material/PowerSettingsNew";
+import Pluton from "./Images/spaceship.jpg";
 
 export default function Navbar() {
   const [profile, setprofile] = useState(true);
   return (
     <>
-      <div className="bg-[#a7c7e7] flex  fixed left-0 top-0 h-[100vh] w-[15vw] md:w-[20vw] flex-col  place-content-around">
+      <div className="bg-[#A7C7E7]  font-semibold flex  fixed left-0 top-0 h-full w-[15vw] md:w-[20vw] flex-col  place-content-around">
         <div className="flex flex-col space-y-[6vh] ">
-          <button className="flex self-center items-center text-xl bg-white w-[7vw] h-[7vw] mx-5  rounded-[50%] ">
-            <img src={zoro} alt="" className="rounded-[50%] w-[7vw] h-[7vw]" />
-            &nbsp;&nbsp;
-            {/* <button>Anant</button> */}
+          <button className="flex items-center flex-col font-bold italic text-3xl mx-2 ">
+            <img src={Pluton} alt="" className=" justify-center md:h-[80px] " />
+
+            <button className="max-lg:hidden font-roboto font-light ">
+              Pluton
+            </button>
           </button>
 
-          <div className="max-[768px]:mx-auto mx-5 flex items-center max-[768px]:justify-center justify-start  text-center  text-xl">
+          <div className="max-md:mx-auto mx-5 flex items-center max-md:justify-center justify-start  text-center  text-xl">
             {" "}
             <HomeIcon className=" scale-[120%]"></HomeIcon>
-            <button className="max-[768px]:hidden">&nbsp;&nbsp;Home</button>
+            <button className="max-md:hidden">&nbsp;&nbsp;Home</button>
           </div>
           {/* <hr className="mt-[3vh] w-[11vw] mx-auto border-black" /> */}
-          <div className="max-[768px]:mx-auto mx-5 flex items-center  max-[768px]:justify-center justify-start text-center  text-xl">
+          <div className="max-md:mx-auto mx-5 flex items-center  max-md:justify-center justify-start text-center  text-xl">
             <LiveHelpIcon className=" scale-[120%]"></LiveHelpIcon>
-            <button className="max-[768px]:hidden">&nbsp;&nbsp;FAQ</button>
+            <button className="max-md:hidden">&nbsp;&nbsp;FAQ</button>
           </div>
           {/* <hr className="mt-[3vh] w-[11vw] mx-auto border-black" /> */}
-          <div className="max-[768px]:mx-auto mx-5 flex items-center max-[768px]:justify-center justify-start  text-center  text-xl">
+          <div className="max-md:mx-auto mx-5 flex items-center max-md:justify-center justify-start  text-center  text-xl">
             <LeaderboardIcon className=" scale-[120%]"></LeaderboardIcon>
-            <button className="max-[768px]:hidden">
-              &nbsp;&nbsp;Leaderboard
-            </button>
+            <button className="max-md:hidden">&nbsp;&nbsp;Leaderboard</button>
           </div>
           {/* <hr className="mt-[3vh] w-[11vw] mx-auto border-black" /> */}
-          <div className="max-[768px]:mx-auto mx-5 flex items-center max-[768px]:justify-center justify-start  text-center  text-xl">
+          <div className="max-md:mx-auto mx-5 flex items-center max-md:justify-center justify-start  text-center  text-xl">
             <InfoIcon className=" scale-[120%]"></InfoIcon>
-            <button className="max-[768px]:hidden">&nbsp;&nbsp;About Us</button>
+            <button className="max-md:hidden">&nbsp;&nbsp;About Us</button>
+          </div>
+          <div className="max-md:mx-auto mx-5 flex items-center max-md:justify-center justify-start  text-center  text-xl">
+            {/* <InfoIcon className=" scale-[120%]"></InfoIcon> */}
+            <button className="flex justify-start self-start  h-[26px] w-[26px] rounded-[100%] items-center text-xl  ">
+              <img
+                src={zoro}
+                alt=""
+                className="h-[26px] w-[26px] rounded-[100%] "
+              />
+              &nbsp;&nbsp;
+              {/* <button>Anant</button> */}
+            </button>
+            <button className="max-md:hidden">&nbsp;&nbsp;Profile</button>
           </div>
         </div>
 
-        <div className="mb-5 max-[768px]:text-center">
-          <div className="max-[768px]:justify-center justify-start text-[#E73A37]  text-xl flex-row">
-            <button className="flex-row  max-[768px]:justify-center justify-start max-[768px]:mx-auto mx-5  ">
-              <div className="flex items-center max-[768px]:justify-center justify-start ">
+        <div className="mb-5 max-md:text-center">
+          <div className="max-md:justify-center justify-start text-[#E73A37]  text-xl flex-row">
+            <button className="flex-row  max-md:justify-center justify-start max-md:mx-auto mx-5  ">
+              <div className="flex items-center max-md:justify-center justify-start ">
                 {/* <div className="scale-[120%] text-[#E73A37]">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -62,9 +76,7 @@ export default function Navbar() {
           
                 </div> */}
                 <PowerSettingsNewIcon />
-                <div className="text-black mx-1 max-[768px]:hidden">
-                  &nbsp;Logout
-                </div>
+                <div className=" mx-1 max-md:hidden">&nbsp;Logout</div>
               </div>
             </button>
           </div>
