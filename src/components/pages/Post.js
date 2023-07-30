129a07bbb2a567629e2ac5d1c652a5c43862d63e
ImDoubD <toodoubd@gmail.com>
2023-07-30 13:26:48 +0530

    dcs

--- src/components/pages/Post.js
+++ src/components/pages/Post.js
@@ -11,8 +11,8 @@ export default function Text() {
   return (
     <div>
       <Navbar></Navbar>
-      <div>
-        <div className="ml-[37vw]  flex items-center mt-[9vh] text-3xl w-[38vw] justify-between max-[710px]:mx-auto max-[619px]:mx-auto max-[420px]:ml-[12vw]">
+      <div className="flex-col max-[710px]:mx-auto  max-[619px]:mx-auto max-[820px]:space-x-100">
+        <div className="ml-[37vw] flex items-center max-[414px]:mt-8 text-3xl w-[38vw] justify-between  max-[420px]:ml-[12vw]">
           <div className="flex-col">
             <button
               onClick={(e) => {
@@ -50,10 +50,10 @@ export default function Text() {
             </button>
           </div>
         </div>
-        <div className="bg-[#4B4A4A] ml-[37vw] h-[22vw] w-[38vw] mt-[3vw] mb-[2vh] max-[420px]:ml-[20vw] max-[420px]:w-[250px] max-[420px]:h-[200px]"></div>
-        <textarea className="bg-[#4B4A4A] ml-[32vw] h-[6vw] w-[50vw] mb-[1vh] text-white p-[0.5vh] max-[420px]:ml-[20vw] max-[420px]:w-[250px]"></textarea>
-      </div>
-      <div className=" max-[823px]:flex-col space-x-2 flex ml-[32vw] text-black text-xl mt-[1vh] max-[700px]:ml-[20vw]">
+        <div className="bg-[#4B4A4A] ml-[37vw] h-[30vh] w-[50vh] mt-[3vw] mb-[2vh] max-[420px]:ml-[20vw] max-[420px]:w-[250px] max-[420px]:h-[200px]"></div>
+        <textarea className="bg-[#4B4A4A] ml-[32vw] h-[10vh] w-[70vh] mb-[1vh] text-white p-[0.5vh] max-[420px]:ml-[20vw] max-[420px]:w-[250px]"></textarea>
+      
+      <div className=" max-[823px]:flex-col space-x-2 flex ml-[32vw] text-black text-xl mt-[3vh] ">
         <div>
           <button
             onClick={(e) => {
@@ -85,11 +85,12 @@ export default function Text() {
           </button>
         </div>
       </div>
+    </div>
       <div
         onClick={(e) => {
           setevent(console.log(4));
         }}
-        className="max-[600px]:ml-[-10vw] ml-[87vw] text-white text-2xl mt-[6vh] hover:font-semibold"
+        className="max-[600px]:mr-[20vw] min-[375px]:ml-[75vw] ml-[100vw] text-white text-2xl mt-[7vh] hover:font-semibold"
       >
         <button>Submit</button>
       </div>
