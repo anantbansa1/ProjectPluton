15a9716dbcddc793d3b24965f4707c904fa4e3d3
Madddynaks <madhav.nakra2002@gmail.com>
2023-07-31 15:05:22 +0530

    aslkdjlkfja

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
 import Poll from "./components/pages/Poll";
-import AddPoll from "./components/pages/AddPoll"
 function App() {
   return (
     <>
-      <AddPoll></AddPoll>
+      <Poll></Poll>
     </>
   );
 }
