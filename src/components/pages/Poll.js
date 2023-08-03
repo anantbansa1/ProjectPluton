54c842b44ebcfe0c00c1a651c3acc5ce92da70fe
Deepanshu <dp5114@dseu.ac.in>
2023-08-03 14:42:45 +0530

    delete

--- src/components/pages/Poll.js
+++ src/components/pages/Poll.js
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import Checkbox from "@mui/material/Checkbox";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import DeleteIcon from '@mui/icons-material/Delete';
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 
 import { green } from "@mui/material/colors";
 import { grey } from "@mui/material/colors";
@@ -10,6 +16,16 @@ function Poll(props) {
   const [option2, setoption2] = useState(false);
   const [option3, setoption3] = useState(false);
   const [option4, setoption4] = useState(false);
+  
+  const [open, setOpen] = useState(false);
+
+  const handleNo = () => {
+    setOpen(false);
+  };
+
+  const handleYes = () => {
+    setOpen(false);
+  };
 
   const handleoption1 = (e) => {
     setoption1(!option1);
@@ -41,13 +57,16 @@ function Poll(props) {
     <div>
       <div className="ml-[20vw] max-md:ml-[15vw] my-10">
         <div className=" mx-auto w-[50vw] max-md:w-[75vw] h-fit bg-[#130f22] shadow-xl rounded-2xl max-md:py-4 py-8 px-4 shadow-black text-white">
-          <div className="flex font-semibold items-center space-x-5">
-            <img
-              src={props.ClubImage}
-              alt=""
-              className=" rounded-[50%] object-cover border-2 border-white h-[2.5vw] w-[2.5vw] min-w-[30px] min-h-[30px]"
-            />
-            <div className="max-md:text-sm">{props.name}</div>
+        <div className="flex justify-between font-semibold items-center ">
+            <div className="flex items-center space-x-5">
+              <img
+                src={props.ClubImage}
+                alt=""
+                className=" rounded-[50%] object-cover border-2 border-white h-[2.5vw] w-[2.5vw] min-w-[30px] min-h-[30px]"
+              />
+              <div className="max-md:text-sm">{props.name}</div>
+            </div>
+            <div className="text-[#962a28] cursor-pointer hover:text-[#c43836] px-4" onClick={()=>{setOpen(true)}}><DeleteIcon className="scale-[130%]" /></div>
           </div>
           <div className="my-3"></div>
           <div className="text-lg max-md:text-sm text-[#dddbdb] ">
@@ -166,6 +185,34 @@ function Poll(props) {
           </div>
         </div>
       </div>
+      <Dialog
+        open={open}
+        PaperProps={{
+          style: {
+            background: "#232323",
+            color: "#fff",
+          },
+        }}
+        // TransitionComponent={Transition}
+        fullWidth
+        maxWidth="sm"
+        keepMounted
+        onClose={handleNo}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle><div className="">{"Delete Poll"}</div></DialogTitle>
+        <DialogContent>
+          <div className="text-[#e4e2e2] text-lg">Are you sure you want to delete?</div>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="" onClick={handleNo}>
+            No
+          </Button>
+          <Button variant="outlined" color="error" onClick={handleYes}>
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
