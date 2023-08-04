4a555189d7afb86851b694a3d8bb0bbbc32ea96d
Samrath <samrathahluwalia999@gmail.com>
2023-08-04 13:55:11 +0530

    d7

--- src/components/AddUserAdmin.js
+++ src/components/AddUserAdmin.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
+import Button from '@mui/material/Button';
 
 function AddAlert(e) {
     alert("Added Successfully")
 }
-
 
 
 export default function AddUserAdmin(props) {
@@ -20,21 +20,25 @@ export default function AddUserAdmin(props) {
         
         <>
             <div>
-                <div className="bg-[#232323] text-white items-center ml-[20vw] mt-[10vh] flex justify-center text-2xl my-10">
+                <div className=" text-white items-center ml-[20vw] mt-[10vh] flex justify-center text-2xl my-10">
                     Add Users
                 </div>
                 <div className="">
-                    <div className="bg-[#232323] text-white flex justify-center ">
-                        <div className="ml-[23vw] py-3" >
-                            To add users upload the an Excel/CSV file in given template <br></br><br></br>
-                            Add a Excel/CSV file:  <strong>{props.filename}</strong>
+                    <div className="ml-[23vw] text-white flex flex-col justify-center ">
+                        <div className="flex py-3 min-[823px]:text-md max-[823px]:flex-col items-center " >
+                            <div className=" max-[823px]:mr-4" >To add users upload the an Excel/CSV file in given template</div> <br></br><br></br>
+                            <button className="rounded-full bg-white text-black px-4 py-2 mx-5 max-[823px]:rounded-3xl">Download Template</button> <br></br> <br></br>
+                            {/* <div className="mx-5">
+                            <Button variant="contained" className="">Download template</Button>
+                            </div> */}
+                            
                         </div>
 
-                        <div className="my-0">
-                            <button className="rounded-full bg-white text-black  px-4 py-2 mx-5">Download Template</button> <br></br> <br></br>
+                        <div className=" my-3 flex py-3 max-[823px]:justify-center">
+                        <div>Add a Excel/CSV file:  <strong>{props.filename}</strong></div>
                         </div>
                     </div>
-                    <div className="bg-[#232323] text-white items-center flex justify-center h-[5vh] ml-[23vw] my-6">
+                    <div className=" text-white items-center flex justify-center h-[5vh] ml-[23vw] my-6 ">
                         <input
                             className=" text-white px-4 py-2 mx-5"
                             type="file"
@@ -43,8 +47,8 @@ export default function AddUserAdmin(props) {
                             onChange={onSelectFile}
                             hidden
                         ></input>
-                        <label for="uploadbtn" className="rounded-full bg-white text-black px-4 py-2 mx-5 cursor-pointer">Upload File</label>
-                        <button className="rounded-full bg-white text-black px-4 py-2 mx-5" onClick={AddAlert}>Confirm</button>
+                        <label for="uploadbtn" className="rounded-full bg-white text-black px-4 py-2 mx-5 cursor-pointer max-[820px]:text-xs">Upload File</label>
+                        <button className="rounded-full bg-white text-black px-4 py-2 mx-5 max-[823px]:text-xs" onClick={AddAlert}>Confirm</button>
                     </div>
 
                 </div>
