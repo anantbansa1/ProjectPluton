86a9fe3910799db59387f1eb9d0f437f2d38aad9
Deepanshu <dp5114@dseu.ac.in>
2023-08-04 20:55:34 +0530

    leaderboard fixed

--- src/components/pages/Leaderboard.js
+++ src/components/pages/Leaderboard.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Navbar from "../Navbar";
 import Row from "./Row";
-import Minions from "../Images/Minions.jpg"
-import Tanjiro from "../Images/Tanjiro.jpg"
 import zoro from "../Images/zoro.jpg"
 import '@fontsource/press-start-2p';
 import rank1 from "../Images/rank1.png"
@@ -22,27 +20,21 @@ export default function Leaderboard(props) {
     <div>
       <Navbar selected="leaderboard">
       </Navbar>
-      <div className=" mt-[3vw] max-md:ml-[15vw] ml-[20vw] text-white">
+      <div className=" max-md:ml-[15vw] ml-[20vw] text-white">
         <div className="font-press-start text-center max-md:text-2xl max-sm:text-xl my-10 text-5xl text-[#FFC700] ">
           Leaderboard
         </div>
         
-        <div className="flex max-md:flex-col max-md:space-y-5  max-md:mx-0 max-xl:text-base items-end h-[300px]  mt-5 mb-10 w-[80vw] md:w-[78vw] space-x-2   ">
-          <div className="w-[33%] max-md:w-[100%]  h-[250px] grid grid-rows-[repeat(5,minmax(40px,auto))] grid-cols-[2fr_1fr_2fr]">
+        <div className="flex max-md:flex-col max-md:space-y-5  max-md:mx-0 max-xl:text-base items-end  mt-5 mb-10 w-[80vw] md:w-[78vw] space-x-2   ">
+          <div className="w-[33%] max-md:w-[100%] max-md:order-2 max-md:h-[200px] h-[250px] grid grid-rows-[repeat(5,minmax(40px,auto))] grid-cols-[2fr_1fr_2fr]">
 
-            <div className="flex flex-col shadow-[#39495f] bg-[#130f22]  h-[230px] shadow-md rounded-lg row-start-2 col-start-1 row-span-4 col-span-3">
+            <div className="flex flex-col shadow-[#39495f] bg-[#130f22] max-md:h-[180px] h-[230px] shadow-md rounded-lg row-start-2 col-start-1 row-span-4 col-span-3">
             </div>
-            <img src={rank2} className="row-start-1 row-span-2 mx-auto col-start-1 col-span-3 h-[80px]" ></img>
+            <img src={rank2} className="row-start-1 row-span-2 mx-auto col-start-1 col-span-3 h-[80px]" alt="" />
             <div className="row-start-3 pr-8 col-span-3 items-center space-x-2 col-start-1 self-center justify-self-center flex ">
-              <img src={zoro} className=" w-[30px] h-[30px] rounded-full border-white"></img>
+              <img src={zoro} className=" w-[30px] h-[30px] rounded-full border-white" alt=""/>
               <span className="text-lg max-md:text-sm font-semibold text-[#fee101] ">Anant Bansal</span>
             </div>
-            {/* <div className="row-start-5 col-start-1 row-span-1 col-span-3 flex justify-around ">
-              <div className="text-[#fee101]">
-                <span className="font-semibold"> Points:</span> <span className="">5000</span>
-              </div>
-
-            </div> */}
             <div className="self-center text-slate-200 font-semibold row-start-4 col-start-3  max-md:text-sm justify-self-end pr-4"> RollNo</div>
             <div className=" self-center row-start-5 col-start-3 text-[#fee101] justify-self-end max-md:text-sm  pr-4">
               41521005
@@ -53,13 +45,13 @@ export default function Leaderboard(props) {
             </div>
 
           </div>
-          <div className="w-[33%] max-md:w-[100%]  h-[300px] grid grid-rows-[repeat(5,minmax(40px,auto))] grid-cols-[2fr_1fr_2fr]">
+          <div className="w-[33%] max-md:w-[100%] max-md:order-1 max-md:h-[200px] h-[300px] grid grid-rows-[repeat(5,minmax(40px,auto))] grid-cols-[2fr_1fr_2fr]">
 
-            <div className="flex flex-col shadow-[#39495f] bg-[#130f22]  h-[280px] shadow-md rounded-lg row-start-2 col-start-1 row-span-4 col-span-3">
+            <div className="flex flex-col shadow-[#39495f] bg-[#130f22] max-md:h-[180px] h-[280px] shadow-md rounded-lg row-start-2 col-start-1 row-span-4 col-span-3">
             </div>
-            <img src={rank1} className="row-start-1 row-span-2 mx-auto  col-start-1 col-span-3 h-[80px]" ></img>
+            <img src={rank1} className="row-start-1 row-span-2 mx-auto  col-start-1 col-span-3 h-[80px]" alt=""/>
             <div className="row-start-3 pr-8 col-span-3 items-center space-x-2 col-start-1 self-center justify-self-center flex ">
-              <img src={zoro} className=" w-[30px] h-[30px] rounded-full border-white"></img>
+              <img src={zoro} className=" w-[30px] h-[30px] rounded-full border-white" alt=""/>
               <span className="text-lg  text-[#fee101] font-bold text-transparent bg-clip-text  bg-gradient-to-r from-red-400 max-md:text-sm   to-yellow-500">Anant Bansal</span>
             </div>
 
@@ -73,13 +65,13 @@ export default function Leaderboard(props) {
             </div>
 
           </div>
-          <div className="w-[33%]  max-md:w-[100%] h-[250px] grid grid-rows-[repeat(5,minmax(40px,auto))] grid-cols-[2fr_1fr_2fr]">
+          <div className="w-[33%]  max-md:w-[100%] max-md:order-3 max-md:h-[200px] h-[250px] grid grid-rows-[repeat(5,minmax(40px,auto))] grid-cols-[2fr_1fr_2fr]">
 
-            <div className="flex flex-col shadow-[#39495f] bg-[#130f22]  h-[230px] shadow-md rounded-lg row-start-2 col-start-1 row-span-4 col-span-3">
+            <div className="flex flex-col shadow-[#39495f] bg-[#130f22] max-md:h-[180px] h-[230px] shadow-md rounded-lg row-start-2 col-start-1 row-span-4 col-span-3">
             </div>
-            <img src={rank3} className="row-start-1 row-span-2 mx-auto col-start-1 col-span-3 h-[80px]" ></img>
+            <img src={rank3} className="row-start-1 row-span-2 mx-auto col-start-1 col-span-3 h-[80px]" alt="" />
             <div className="row-start-3 pr-8 col-span-3 items-center space-x-2 col-start-1 self-center justify-self-center flex ">
-              <img src={zoro} className=" w-[30px] h-[30px] rounded-full border-white"></img>
+              <img src={zoro} className=" w-[30px] h-[30px] rounded-full border-white" alt=""/>
               <span className="text-lg font-semibold text-[#fee101] max-md:text-sm  ">Anant Bansal</span>
             </div>
             {/* <div className="row-start-5 col-start-1 row-span-1 col-span-3 flex justify-around ">
@@ -99,7 +91,7 @@ export default function Leaderboard(props) {
 
           </div>
         </div>
-        <div className="w-[85vw] md:w-[78vw] pl-2 max-sm:p-0">
+        <div className="w-[82vw] md:w-[78vw] pl-2 max-sm:p-0">
 
           <div className="bg-[#0b0914] py-6 max-sm:text-xs max-sm:gap-1  rounded-lg max-sm:p-0 grid  grid-cols-[10%_10%_35%_22.5%_22.5%]  max-md:text-base text-xl max-sm:mr-0  text-white">
             <div className="font-semibold text-center ">Medal</div>
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -258,7 +258,7 @@ function Login() {
         </div>
       </div> */}
       <Backdrop
-        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1, backdropFilter: "blur(20px)", }}
         open={Loading}
         close={Loading}
       >
