388e73b2373983ee02a9eeb087a985992f92b268
Madddynaks <madhav.nakra2002@gmail.com>
2023-08-04 17:13:40 +0530

    ewjfklsjdf

--- src/components/pages/FAQ.js
+++ src/components/pages/FAQ.js
@@ -10,7 +10,7 @@ export default function FAQ() {
       content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
           laborum cupiditate possimus labore, hic temporibus velit dicta earum
           suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-          voluptatem.`,
+          voluptatem. madhav`,
     },
     {
       title: "Q. What do badges mean?",
