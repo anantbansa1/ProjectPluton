a65ed42e9d8deba639f32c9fe12cff3e329e2be9
Deepanshu <dp5114@dseu.ac.in>
2023-08-07 00:42:29 +0530

    manageusers

--- src/components/pages/AddUserAdmin.js
+++ src/components/pages/AddUserAdmin.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Papa from "papaparse";
 import Navbar from "../Navbar";
 import { doc, setDoc } from "firebase/firestore";
@@ -9,19 +9,30 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { db } from "../../firebase";
 import Backdrop from "@mui/material/Backdrop";
 import CircularProgress from "@mui/material/CircularProgress";
+import { Button } from "@mui/material";
 
 export default function AddUserAdmin(props) {
   const allowedExtensions = ["csv"];
   const [file, setFile] = useState([]);
   const [loading, setloading] = useState(false);
   const [open, setOpen] = useState(false);
+  const [valueArray, setvaluearray] = useState([]);
+  const [filename, setfilename] = useState("No file selected");
+  const [columnArray, setcolumnarray] = useState([]);
   const [etype, setetype] = useState("success");
   const [message, setmessage] = useState("Successfully Added!");
   const [isfile, setisfile] = useState(0);
+  const [add, setadd] = useState(true);
+  const inputaddref = useRef();
 
   const Alert = React.forwardRef(function Alert(props, ref) {
     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
   });
+
+  // useEffect(() => {
+  //   console.log(values);
+  // }, [values])
+
 
   const handleClose = (event, reason) => {
     if (reason === "clickaway") {
@@ -69,8 +80,24 @@ export default function AddUserAdmin(props) {
         return;
       }
       setFile(inputFile);
+      setfilename(e.target.files[0].name);
+      Papa.parse(e.target.files[0], {
+        header: true,
+        skipEmptyLines: true,
+        complete: function (result) {
+          const columnarray = [];
+          const valuearray = [];
+
+          result.data.map((d) => {
+            columnarray.push(Object.keys(d))
+            valuearray.push(Object.values(d))
+          })
+          setcolumnarray(columnarray)
+          setvaluearray(valuearray)
+        }
+      })
     }
-  };
+  }
 
   function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
@@ -94,6 +121,7 @@ export default function AddUserAdmin(props) {
       });
       parsedData.map(async (d) => {
         const data = Object.values(d);
+
         const docref = doc(db, "user", data[0]);
         const payload = {
           name: data[1],
@@ -112,46 +140,87 @@ export default function AddUserAdmin(props) {
 
   return (
     <>
-      <Navbar selected="Adduser"></Navbar>
-      <div>
-        <div className=" text-white items-center ml-[20vw] mt-[10vh] flex justify-center text-2xl my-10">
-          Add Users
+      <Navbar selected="manageusers"></Navbar>
+
+      <div className=" md:ml-[22vw] flex flex-col space-y-5 max-md:w-[85%] text-white  ml-[18vw] my-[2vw] mr-[2vw] bg-[#130f22b6] shadow-xl rounded-2xl py-8 px-4 shadow-black">
+        <div className="text-3xl max-md:text-xl">Manage Users</div>
+        <div className="flex space-x-5 self-center text-2xl max-md:text-lg text-slate-200">
+          <button onClick={() => { setadd(!add) }} className={`px-6 py-4 ${add ? 'border-b' : ''} border-slate-200`} >Add Users</button>
+          <button onClick={() => { setadd(!add) }} className={`px-6 py-4 ${!add ? 'border-b' : ''}  border-slate-200`} > Remove Users</button>
         </div>
-        <div className="">
-          <div className="ml-[23vw] text-white flex flex-col justify-center ">
-            <div className="flex py-3 min-[823px]:text-md max-[823px]:flex-col items-center ">
-              <div className=" max-[823px]:mr-4">
-                To add users upload the an Excel/CSV file in given template
-              </div>{" "}
-              <br></br>
-              <br></br>
-              <button className="rounded-full bg-white text-black px-4 py-2 mx-5 max-[823px]:rounded-3xl">
-                Download Template
-              </button>{" "}
-              <br></br> <br></br>
-            </div>
-            <div className=" my-3 flex py-3 max-[823px]:justify-center">
-              <div>
-                Add a Excel/CSV file: <strong>{props.filename}</strong>
+        <div>
+          {add && (
+            <div className="mt-10">
+              <div className="flex space-x-10 text-xl max-md:text-base">
+                <div>
+                  To add users upload the an CSV file in given template</div>
+                <div>
+                  <Button
+
+                    variant="contained"
+                    color="primary"
+                    sx={{ background: "#15803d", color: 'white', background: "#100d1e", borderColor: '#199245', "&:hover": { background: "#100d1e", borderColor: '#0a0813', color: 'white' } }}
+
+                  >
+                    Download Template
+                  </Button>
+                </div>
+              </div>
+              <div className="my-5 flex space-x-5 gap-x-2 items-center">
+                Add CSV file
+                <Button
+                  className="mx-5"
+                  variant="contained"
+                  onClick={() => { inputaddref.current.click(); console.log("hello") }}
+                  color="primary"
+                  sx={{ background: "#15803d", color: 'white', background: "#100d1e", borderColor: '#199245', "&:hover": { background: "#100d1e", borderColor: '#0a0813', color: 'white' } }}
+
+                >
+                  Select file
+                </Button>
+                <div>{filename}</div>
+                <input
+                  ref={inputaddref}
+                  className="hidden text-[#5d5d5d] file:mr-5 file:px-4 file:py-2 file:border-[1px] file:text-xs file:font-medium file:bg-black file:text-white hover:file:cursor-pointer hover:file:bg-black hover:file:text-white"
+                  type="file"
+                  accept="csv"
+                  onChange={onSelectFile}
+                ></input>
+                <Button
+                  className="mx-5"
+                  variant="contained"
+                  color="primary"
+                  onClick={handleParse}
+                  sx={{ background: "#15803d", color: 'white', background: "#100d1e", borderColor: '#199245', "&:hover": { background: "#100d1e", borderColor: '#0a0813', color: 'white' } }}
+
+                >
+                  upload
+                </Button>
               </div>
             </div>
-          </div>
-          <div className=" text-white items-center flex justify-center h-[5vh] ml-[23vw] my-6 ">
-            <input
-              className=" text-[#5d5d5d] file:mr-5 file:px-4 file:py-2 file:border-[1px] file:text-xs file:font-medium file:bg-black file:text-white hover:file:cursor-pointer hover:file:bg-black hover:file:text-white"
-              type="file"
-              accept="csv"
-              onChange={onSelectFile}
-            ></input>
-            <button
-              className="rounded-full bg-white text-black px-4 py-2 mx-5 max-[823px]:text-xs"
-              onClick={handleParse}
-            >
-              Confirm
-            </button>
-          </div>
+          )}
         </div>
       </div>
+      <div className=" md:ml-[22vw] flex flex-col space-y-5 max-md:grid-cols-[repeat(4,minmax(auto,20vw))] text-white  ml-[18vw] my-[2vw] mr-[2vw] bg-[#130f22b6] shadow-xl rounded-2xl py-8 px-4 shadow-black">
+        <div className="grid gap-y-2 text-slate-200  grid-cols-[1fr_1fr_2.5fr_3fr] striped text-lg max-md:text-sm max-lg:text-base">
+          <div className="row-start-1 col-start-1 font-semibold text-xl max-md:text-sm max-lg:text-base bg-[#100d1e] p-2 rounded-lg text-center">S.No</div>
+          <div className="row-start-1 col-start-2 font-semibold text-xl max-md:text-sm max-lg:text-base bg-[#100d1e] p-2 rounded-lg text-center">Roll No</div>
+          <div className="row-start-1 col-start-3 font-semibold text-xl max-md:text-sm max-lg:text-base bg-[#100d1e] p-2 rounded-lg text-center">Name</div>
+          <div className="row-start-1 col-start-4 font-semibold text-xl  max-md:text-sm max-lg:text-base bg-[#100d1e] p-2 rounded-lg text-center">Email ID</div>
+
+          {valueArray.map((value, index) => {
+            return (
+              <>
+                <div className={`row-start-${index + 2} col-start-1 p-2 rounded-lg text-center`}>{index + 1}</div>
+                <div className={`row-start-${index + 2} col-start-2 p-2 rounded-lg text-center`}>{value[0]}</div>
+                <div className={`row-start-${index + 2} col-start-3 p-2 rounded-lg text-center`}>{value[1]}</div>
+                <div className={`row-start-${index + 2} col-start-4 p-2 rounded-lg text-center`}>{value[2]}</div>
+              </>
+            )
+          })}
+        </div>
+      </div>
+
       <Backdrop
         sx={{
           color: "#fff",
